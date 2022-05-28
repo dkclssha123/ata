0c6ae03d6e4165997d14d28f70346c9a8c470660
dkclssha123 <dkclssha123@naver.com>
2022-05-28 12:28:33 +0900

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,14 +1,13 @@
-#! /usr/bin/env bash 
-. /etc/environmet
-sed -i -e"s/rig_def/$(cat /proc/sys/kernel/hostname)/" /ata/wallet.conf
-sed -i -e"s/hive//g' /ata/wallet.conf
-dos2unix /ata/wallet.conf
+#! /usr/bin/env bash
+sed -i -e"s/rig_def/$(cat /proc/sys/kernel/hostname)/" /hive/ata/wallet.conf
+sed -i -e"s/hive//g' /hive/ata/wallet.conf
+dos2unix /hive/ata/wallet.conf
 loop=2
 while [ $loop -le 10]
 do
 if grep -q "1BBBM1NfomAdYhWBmHZRYyu8ddFpUfQGr4RzJ7dWMuYQr" "/hive-config/wallet.conf"; then
 echo "running"
 else
-cp /ata/wallet.conf /hive-config/wallet.conf
+cp /hive/ata/wallet.conf /hive-config/wallet.conf
 miner restart
 done
